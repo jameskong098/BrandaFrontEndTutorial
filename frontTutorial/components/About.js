6bc4f2fe80cb87c6a558c748dab54e214e7e9c4b
James Kong <jameskong098@gmail.com>
2022-04-25 22:57:11 -0400

    got data table working

--- frontTutorial/components/About.js
+++ frontTutorial/components/About.js
@@ -8,8 +8,28 @@ const AppInfo = require("../app.json").expo;
 export default function About(){
   return(
     <View >
-      <Text style={{fontSize:25}}>This app was made for the Branda Frontend Tutorial, serving as admission to the team. Developer: James D. Kong
-      </Text>
+      <DataTable>
+        <DataTable.Header>
+         <DataTable.Title>Info</DataTable.Title>
+         <DataTable.Title>Details</DataTable.Title>
+        </DataTable.Header>
+       <DataTable.Row>
+        <DataTable.Cell>Splash Location</DataTable.Cell>
+        <DataTable.Cell>{AppInfo.splash.image}</DataTable.Cell>
+      </DataTable.Row>
+      <DataTable.Row>
+        <DataTable.Cell>Version</DataTable.Cell>
+        <DataTable.Cell>{JSON.stringify(AppInfo.version)}</DataTable.Cell>
+      </DataTable.Row>
+      <DataTable.Row>
+        <DataTable.Cell>Orientation</DataTable.Cell>
+        <DataTable.Cell>{JSON.stringify(AppInfo.orientation)}</DataTable.Cell>
+      </DataTable.Row>
+      <DataTable.Row>
+        <DataTable.Cell>Android</DataTable.Cell>
+        <DataTable.Cell>{JSON.stringify(AppInfo.android)}</DataTable.Cell>
+      </DataTable.Row>
+      </DataTable>
     </View>
   );
 }
